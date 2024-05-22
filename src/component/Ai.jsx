96fd3473ecb721333402a67fbba7e742e8cb5f39
land-bit <landrybitege7@gmail.com>
2024-05-22 14:53:14 +0200

    Add wikidata query service

--- src/component/Ai.jsx
+++ src/component/Ai.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-
-export default function Ai (){
-    return (
-        <div className="w-[50%] bg-slate-300">
-            <h1>Artificial Intelligence</h1>
-        </div>
-    )
+export default function Ai() {
+  return (
+    <div className="w-[30%] bg-slate-300">
+      <h1>Artificial Intelligence</h1>
+    </div>
+  );
 }
