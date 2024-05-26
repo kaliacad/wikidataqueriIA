97bbda1fa60badb25dd2e2ac27f9a741f68fbc46
BirushaNdegeya <birushandegeya@gmail.com>
2024-05-26 19:33:50 +0200

    finish layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
+import { useState, useEffect } from 'react';
 import Ai from './components/Ai';
 import Query from './components/Query';
 import Resultat from './components/Resultat';
 import Split from 'react-split';
 import Footer from './components/Footer';
-
+import getSPARQL from './api/getSPARQL';
 
 
 function App() {
+  const [text, setText] = useState('');
+  useEffect(() => {
+    async function loadAPI() {
+      try {
+        const resp = await getSPARQL('je vais les femmes politiciennes en RDC donnez moi le code SPARQL en commentaire');
+        setText(resp);
+      } catch (err) {
+        console.log({ message: err.stack });
+      }
+    }
+    loadAPI();
+  }, [])
   return (
     <main role='main' className='h-screen w-[100%] flex flex-col'>
       <div className='flex-1 h-[100%]'>
         <Split className="split">
           <div className='flex flex-col'>
-            <Query text={'something new about programming'} />
-            <Ai text={'something new about programming'} />
+            <Query text={text} />
+            <Ai text={'je vais les femmes politiciennes en RDC donnez moi le code SPARQL en commentaire'} />
           </div>
           <Resultat />
         </Split>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
 export default function Header() {
-   const color = 'bg-slate-700'
    return (
       <header className="bg-[#eee] h-[50px] flex justify-between items-center text-slate-700 pr-3">
-         <button className="bg-slate-700 text-white px-24 h-10 font-bold">RUN</button>
+         <button className="bg-slate-700 text-white px-24 h-10 font-bold hover:text-slate-400">RUN</button>
          <h1 className="text-3xl font-semibold">Wikidata Queri AI</h1>
       </header>
    );
