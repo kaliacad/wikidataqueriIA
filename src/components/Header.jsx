2c67e2d394a38680639d9fd82195e16a3801d55e
PatriceKalwira <patricekalwira@gmail.com>
2024-05-29 15:54:22 +0200

    changement du nom de l'application

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 export default function Header() {
-   return (
-      <header className="bg-[#eee] h-[50px] flex justify-between items-center text-slate-700 pr-3">
-         <button className="bg-slate-700 text-white px-24 h-10 font-bold hover:text-slate-400">RUN</button>
-         <h1 className="text-3xl font-semibold">Wikidata Queri AI</h1>
-      </header>
-   );
+  return (
+    <header className="bg-[#eee] h-[50px] flex justify-between items-center text-slate-700 pr-3">
+      <button className="bg-slate-700 text-white px-24 h-10 font-bold hover:text-slate-400">
+        RUN
+      </button>
+      <h1 className="text-3xl font-semibold">Wikidata Query AI</h1>
+    </header>
+  );
 }
